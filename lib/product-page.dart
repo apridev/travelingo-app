34143acbd139127370ffbe8d33bf73ad9c18b637
Apridev <apriantofajar11@gmail.com>
2022-06-24 14:02:51 +0900

    product list

---
+++ lib/product-page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:travelingo_apps/template.dart';
+
+final List<Map<String, dynamic>> myList =[
+  {
+    "nama": "Eiffel Tower",
+    "namaTempat": "Paris, France",
+    "rating": "assets/icons/icon-star.png",
+    "harga": 381,
+    "gambar": "assets/imgs/product1.png"
+  },
+  {
+    "nama": "Sakura Japan Streetart",
+    "namaTempat": "Tokyo, Japan",
+    "rating": "assets/icons/icon-star.png",
+    "harga": 381,
+    "gambar": "assets/imgs/product2.png"
+  },
+  {
+    "nama": "Alhambra Granada",
+    "namaTempat": "Madrid, Spanyol",
+    "rating": "assets/icons/icon-star.png",
+    "harga": 381,
+    "gambar": "assets/imgs/product3.png"
+  },
+];
+
+final List<Map<String, dynamic>> listRecommended = [
+  {
+    "gambar": "assets/imgs/product02.png",
+    "namaTempat": "Tower Bridge",
+    "tempat": "London, United Kingdom",
+    "harga": 512
+  },
+  {
+    "gambar": "assets/imgs/product01.png",
+    "namaTempat": "Tower Bridge",
+    "tempat": "Berlin, Germany",
+    "harga": 341
+  },
+  {
+    "gambar": "assets/imgs/product02.png",
+    "namaTempat": "Tower Bridge",
+    "tempat": "Tokyo, Japan",
+    "harga": 431
+  },
+];
