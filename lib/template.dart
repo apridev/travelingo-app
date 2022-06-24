259f41de76ea4b6aaa84d8556174b6dc2fbde675
Apridev <apriantofajar11@gmail.com>
2022-06-24 14:03:04 +0900

    template page

---
+++ lib/template.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 30.0;
+double defaultHome = 20.0;
+
+Color primaryTextColor = Color(0xff1B7FC5);
+Color BlackTextColor = Color(0xff354259);
+Color BackgroundTextColor = Color(0xffffffff);
+Color BackgroundTextColor1 = Color(0xffFAFAFA);
+Color inputTextColor = Color(0xffD0C9C0);
+Color GoogleTextColor = Color(0xffE05D5D);
+Color FacebookTextColor = Color(0xff1B7FC5);
+Color starTextColor = Color(0xffF2C935);
+
+TextStyle primaryTextStyle = GoogleFonts.kanit(
+  color: primaryTextColor
+);
+
+TextStyle BlackTextStyle = GoogleFonts.kanit(
+  color: BlackTextColor
+);
+
+TextStyle BackgroundTextStyle = GoogleFonts.kanit(
+  color: BackgroundTextColor
+);
+
+TextStyle GoogleTextStyle = GoogleFonts.kanit(
+  color: GoogleTextColor
+);
+
+TextStyle FacebookTextStyle = GoogleFonts.kanit(
+  color: FacebookTextColor
+);
+
+FontWeight reguler = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
