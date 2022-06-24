0cd58d90af530be959a1e05fee299b887774b499
Apridev <apriantofajar11@gmail.com>
2022-06-24 14:02:41 +0900

    main page

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:travelingo_apps/home/main-page.dart';
+import 'package:travelingo_apps/pages/forget-password.dart';
+import 'package:travelingo_apps/pages/login-page.dart';
+import 'package:travelingo_apps/pages/register-page.dart';
+import 'package:travelingo_apps/pages/verification-account.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp
+  ]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     routes: {
+      '/':(context) => LoginPage(),
+      '/register':(context) => RegisterPage(),
+      '/forget-password':(context) => ForgetPasswordPage(),
+      '/verification-page':(context) => VerificationAccount(),
+      '/main-page-home':(context) => MainPageHome(),
+     },
+    );
+  }
+}
