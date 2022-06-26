f7ad87608daf9316ff86568ab8a8f3696d1792e3
Apridev <apriantofajar11@gmail.com>
2022-06-26 19:00:25 +0900

    wishlist page

---
+++ lib/home/wishlist-page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class WishlistPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text('Wishlist Page'),
+          ),
+        ],
+      ),
+    );
+  }
+}
