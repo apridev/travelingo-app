53a5ad430b78473626f31eef4a5b88d6bc1d8f11
Apridev <apriantofajar11@gmail.com>
2022-06-26 19:00:56 +0900

    invest page

---
+++ lib/home/invest-page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class InvestPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text('Invest Page'),
+          ),
+        ],
+      ),
+    );
+  }
+}
